0f6f9f045e5e5da1d5ba60db4f26aa8e6a9894cf
xavierfranquetd-make <xavierfranquetd@gmail.com>
2026-03-13 17:39:50 -0400

    Remove social links from navbar
    Removed Twitter and GitHub links from the navbar.

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -83,13 +83,6 @@ export const Navbar = ({ navItems, navMenuItems }: NavbarProps) => {
         className='hidden md:flex basis-1/5 sm:basis-full'
         justify='end'
       >
-        <NavbarItem className='hidden sm:flex gap-2'>
-          <Link isExternal href={siteConfig.links.twitter} aria-label='Twitter'>
-            <TwitterIcon className='text-default-500' />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} aria-label='Github'>
-            <GithubIcon className='text-default-500' />
-          </Link>
           <ThemeSwitch />
         </NavbarItem>
         <NavbarItem>
