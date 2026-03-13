44a6f4a104d12465d2c95580e25a2837ab1b3748
xavierfranquetd-make <xavierfranquetd@gmail.com>
2026-03-13 19:14:46 -0400

    Refactor language entries in site configuration

--- web/src/config/site.ts
+++ web/src/config/site.ts
@@ -15,40 +15,8 @@ export type Language = {
 
 export const languages: Language[] = [
   { code: 'en', name: 'English', countryCode: 'us', map: ['en-GB'] },
-  { code: 'ar', name: 'Arabic', map: ['ar-sa'] },
-  { code: 'de', name: 'German', countryCode: 'de', map: ['de-DE'] },
-  {
-    code: 'es',
-    name: 'Spanish',
-    countryCode: 'es',
-    map: ['es-ES', 'es-US', 'es-MX', 'ca']
-  },
-  { code: 'fr', name: 'French', countryCode: 'fr', map: ['fr-FR', 'fr-CA'] },
-  { code: 'id', name: 'Indonesian', countryCode: 'id' },
-  { code: 'it', name: 'Italian', countryCode: 'it', map: ['it-IT'] },
-  { code: 'no', name: 'Norwegian', countryCode: 'no', map: ['nb', 'nn'] },
-  {
-    code: 'pt',
-    name: 'Portuguese',
-    countryCode: 'pt',
-    map: ['pt-BR', 'pt-PT']
-  },
-  { code: 'sv', name: 'Swedish', countryCode: 'se' },
-  { code: 'uk', name: 'Ukrainian', countryCode: 'ua' },
-  { code: 'da', name: 'Danish', countryCode: 'dk' },
-  { code: 'fi', name: 'Finnish', countryCode: 'fi' },
-  { code: 'hi', name: 'Hindi', countryCode: 'in' },
-  { code: 'is', name: 'Icelandic', countryCode: 'is' },
-  { code: 'ja', name: 'Japanese', countryCode: 'jp' },
-  { code: 'pl', name: 'Polish', countryCode: 'pl' },
-  { code: 'ru', name: 'Russian', countryCode: 'ru' },
-  { code: 'th', name: 'Thai', countryCode: 'th' },
-  {
-    code: 'zh',
-    name: 'Chinese',
-    countryCode: 'cn',
-    map: ['zh-CN', 'zh-Hans', 'zh-Hant']
-  }
+  { code: 'es', name: 'Spanish', countryCode: 'es', map: ['es-ES', 'es-US', 'es-MX', 'ca'] },
+  { code: 'pt', name: 'Portuguese', countryCode: 'pt', map: ['pt-BR', 'pt-PT'] },
 ];
 
 export const locales = languages.map((lang) => lang.code) as string[];
