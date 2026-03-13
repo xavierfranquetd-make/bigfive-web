38187aa464b2788e74b8f359b8825bc680492f3c
xavierfranquetd-make <xavierfranquetd@gmail.com>
2026-03-12 23:22:39 -0400

    Refactor database connection handling

--- web/src/db/index.ts
+++ web/src/db/index.ts
@@ -1,20 +1,23 @@
 import mongodb, { MongoClient } from 'mongodb';
 
-const url = process.env.DB_URL;
-if (!url) {
-  throw new Error(
-    'Please define the DB_URL environment variable inside .env.local'
-  );
-}
-
 const dbName = process.env.DB_NAME || 'results';
 
-const mongoClient = new MongoClient(url);
-
 let cachedDb: mongodb.Db | null = null;
+let mongoClient: MongoClient | null = null;
 
 export async function connectToDatabase() {
   if (cachedDb) return cachedDb;
+
+  const url = process.env.DB_URL;
+  if (!url) {
+    throw new Error(
+      'Please define the DB_URL environment variable inside .env.local'
+    );
+  }
+
+  if (!mongoClient) {
+    mongoClient = new MongoClient(url);
+  }
 
   const client = await mongoClient.connect();
   const db = client.db(dbName);
