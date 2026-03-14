a37369f28f5c6271372e6c5bbdcbbbf96c9f4cc0
xavierfranquetd-make <xavierfranquetd@gmail.com>
2026-03-13 22:23:15 -0400

    Refactor Results component and clean up imports
    Refactor Results component and remove unused imports.

--- web/src/app/[locale]/result/[id]/page.tsx
+++ web/src/app/[locale]/result/[id]/page.tsx
@@ -1,18 +1,9 @@
 import { Report, getTestResult } from '@/actions';
-import { Snippet } from '@nextui-org/snippet';
 import { useTranslations } from 'next-intl';
-import { title } from '@/components/primitives';
-import { DomainPage } from './domain';
-import { Domain } from '@bigfive-org/results';
 import { getTranslations } from 'next-intl/server';
-import { BarChart } from '@/components/bar-chart';
-import { Link } from '@/navigation';
-import { ReportLanguageSwitch } from './report-language-switch';
 import { Alert } from '@/components/alert';
 import { supportEmail } from '@/config/site';
-import ShareBar from '@/components/share-bar';
-import { DomainTabs } from './domain-tabs';
-import { Chip } from '@nextui-org/react';
+import { Results } from './results';
 
 export async function generateMetadata({
   params: { locale }
@@ -36,7 +27,6 @@ export default async function ResultPage({
   searchParams
 }: ResultPageParams) {
   let report;
-
   try {
     report = await getTestResult(params.id.substring(0, 24), searchParams.lang);
   } catch (error) {
@@ -55,57 +45,3 @@ export default async function ResultPage({
 
   return <Results report={report} showExpanded={searchParams.showExpanded} />;
 }
-
-interface ResultsProps {
-  report: Report;
-  showExpanded?: boolean;
-}
-
-const Results = ({ report, showExpanded }: ResultsProps) => {
-  const t = useTranslations('results');
-
-  return (
-    <>
-      <div className='flex'>
-        <div className='flex-grow'>
-          <ReportLanguageSwitch
-            language={report.language}
-            availableLanguages={report.availableLanguages}
-          />
-        </div>
-        <Chip>{new Date(report.timestamp).toLocaleDateString()}</Chip>
-      </div>
-      <div className='text-center mt-4'>
-        <span className='font-bold'>{t('important')}</span> &nbsp;
-        {t('saveResults')} &nbsp;
-        <Link href={`/compare/?id=${report.id}`} className='underline'>
-          {t('compare')}
-        </Link>{' '}
-        &nbsp;
-        {t('toOthers')}
-      </div>
-      <div className='flex mt-4'>
-        <Snippet
-          hideSymbol
-          color='danger'
-          className='w-full justify-center'
-          size='lg'
-        >
-          {report.id}
-        </Snippet>
-      </div>
-      <div className='flex mt-5 justify-end w-full gap-x-1 print:hidden'>
-        <ShareBar report={report} />
-      </div>
-      <div className='flex mt-10'>
-        <h1 className={title()}>{t('theBigFive')}</h1>
-      </div>
-      <BarChart max={120} results={report.results} />
-      <DomainTabs
-        results={report.results}
-        showExpanded={!!showExpanded}
-        scoreText={t('score')}
-      />
-    </>
-  );
-};
